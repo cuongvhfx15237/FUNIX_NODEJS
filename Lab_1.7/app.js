9159eb1dcc417047b794ca8038699d6ed4c8eb12
Vo Huy Cuong <cuongvh_fx15237@funix.edu.vn>
2022-07-28 03:06:25 +0700

    done lab 1.7

--- Lab_1.7/app.js
+++ Lab_1.7/app.js
@@ -1,41 +1,9 @@
-const fs = require('fs');
 const http = require('http');
 
-const server = http.createServer((req, res)=>{
+const routes = require('./routes')
 
-    const url = req.url;
-    const method = req.method;
-
-    if(url === '/'){
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><p>Test install nodemon changes server</p><input type="text" name="message"><button type="submit">SUbmit</button></form>></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if(url === "/message" && method === "POST"){
-        const body = [];
-        req.on('data', (chunk)=>{
-            console.log(chunk)
-            body.push(chunk);
-        })
-        req.on('end', () =>{
-            const parsebody = Buffer.concat(body).toString();
-            const message = parsebody.split('=')[1];
-            fs.writeFileSync("message.txt", message);
-        })
-
-        res.statusCode = 302;
-        res.setHeader("location","/")
-        return res.end();
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+console.log(routes.someText)
+const server = http.createServer(routes.handler);
 
 // http.createServer(rqListener);
 server.listen(3000);
